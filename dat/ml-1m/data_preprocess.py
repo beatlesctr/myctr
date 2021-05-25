82c4dd265f4090a5d4c6c107cd392187729feb2a
gavincaoyuji <gavincaoyuji@didiglobal.com>
2021-05-25 23:32:18 +0800

    优化 ctr 数据

--- dat/ml-1m/data_preprocess.py
+++ dat/ml-1m/data_preprocess.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 import pandas as pd
 
-user = pd.read_table(filepath_or_buffer='.\\raw\\users.dat', sep='::')
-movie = pd.read_table(filepath_or_buffer='.\\raw\\movies.dat', sep='::')
+user = pd.read_table(filepath_or_buffer='./raw/users.dat', sep='::')
+movie = pd.read_table(filepath_or_buffer='./raw/movies.dat', sep='::')
 def process_genres(genres):
     tmp = genres.split('|')
     while len(tmp) < 3:
@@ -12,18 +12,24 @@ def process_genres(genres):
 movie['Genres'] = movie.apply(lambda x: process_genres(x['Genres']), axis=1)
 movie[['Genres1', 'Genres2', 'Genres3']] = movie['Genres'].str.split('%', 2, expand=True)
 del movie['Genres']
-rating = pd.read_table(filepath_or_buffer='.\\raw\\ratings.dat', sep='::')
+rating = pd.read_table(filepath_or_buffer='./raw/ratings.dat', sep='::')
 print(rating.head(10))
 result = pd.merge(left=rating, right=user, on='UserID', how='left')
 result = pd.merge(left=result, right=movie, on='MovieID', how='left')
-result = result.sort_values(by='Timestamp')
+result = result.sort_values(by='Timestamp')[lambda x : x['Rating'] != 3]
+del result['Timestamp']
 
+result['Rating'] = result.apply(lambda x: 0 if int(x['Rating']) < 3 else 1, axis=1)
 sparse_cols = ["UserID","MovieID","Gender","Age","Occupation","Zip-code","Title","Genres1","Genres2","Genres3"]
 
 sparse_feat_space_cfg = list()
 for ele in sparse_cols:
     sparse_feat_space_cfg.append(len(set(result[ele])))
 print(sparse_feat_space_cfg)
+
+import random
+index = [i for i in range(len(result))]
+random.shuffle(index)
 
 train_set_size = int(len(result) * 9 / 10)
 test_set_size = int(len(result) / 10)
